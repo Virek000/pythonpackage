4cc400be416e7fa2d8d229f3379399faaa1b8948
Virek000 <48675848+Virek000@users.noreply.github.com>
2019-03-19 14:49:21 +0200

    Update sorting.py

--- pythonpackage/sorting.py
+++ pythonpackage/sorting.py
@@ -11,35 +11,32 @@ def bubble_sort(items):
 
 
 def merge_sort(items):
+    def merge(A, B):    
+        new_list = []
+        while len(A) > 0 and len(B) > 0:
+            if A[0] < B[0]:
+                new_list.append(A[0])
+                A.pop(0)
+            else:
+                new_list.append(B[0])
+                B.pop(0)
 
-    '''Return array of items, sorted in ascending order'''
-    nl = []
-    while len(A) > 0 and len(B) > 0:
-        if A[0] < B[0]:
-            nl.append(A[0])
-            A.pop(0)
-        else:
-            nl.append(B[0])
-            B.pop(0)
+        if len(A) == 0:
+            new_list = new_list + B    
+        if len(B) == 0:
+            new_list = new_list + A
 
-    if len(A) == 0:
-        nl = nl + B
-    if len(B) == 0:
-        nl = nl + A
-
-    return nl
-
-
-def merge_sort(items):
-
+        return new_list        
+            
+    
     len_i = len(items)
     if len_i == 1:
-        return items
-
+        return items       
+        
     mid_point = int(len_i / 2)
     i1 = merge_sort(items[:mid_point])
-    i2 = merge_sort(items[mid_point:])
-
+    i2 = merge_sort(items[mid_point:])       
+    
     return merge(i1, i2)
 
 
@@ -51,7 +48,7 @@ def quick_sort(items):
     if len_i <= 1:
         return items
 
-    pivot = items[index]
+    pivot = items[-1]
     small = []
     large = []
     dup = []
